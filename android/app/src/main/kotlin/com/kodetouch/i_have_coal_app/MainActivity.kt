364c6c53b78a7353f84221986746945f4741d3a2
Ziky19 <sussananukem20@gmail.com>
2023-01-20 13:32:14 +0000

    Initial commit

---
+++ android/app/src/main/kotlin/com/kodetouch/i_have_coal_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kodetouch.i_have_coal_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: kDebugMode,
+      builder: (context) => const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('I Have Coal'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      backgroundColor: Colors.cyan[700],
+      body: const Center(
+        child: Image(
+          image: AssetImage(
+            'images/coal.png',
+          ),
+          width: 300.0,
+          height: 300.0,
+        ),
+      ),
+    );
+  }
+}
